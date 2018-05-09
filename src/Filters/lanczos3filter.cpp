1ac5b5c21ec87de4c925fcedc62c0382ff0fab2a
amichaeltm <38765529+amichaeltm@users.noreply.github.com>
2018-05-09 14:37:51 -0500

    Use C++11 function names

--- src/Filters/lanczos3filter.cpp
+++ src/Filters/lanczos3filter.cpp
@@ -21,7 +21,7 @@
 #include <cmath>
 
 Lanczos3Filter::Lanczos3Filter() :
-    PI_FLOAT(std::atanf(1.0) * 4)
+    PI_FLOAT(std::atan(1.0f) * 4)
 {
 }
 
@@ -37,7 +37,7 @@ float Lanczos3Filter::value(float value)
     {
         value *= PI_FLOAT;
         float value_divided_by_3 = value / 3.0f;
-        return (std::sinf(value) / value) * (std::sinf(value_divided_by_3) / value_divided_by_3);
+        return (std::sin(value) / value) * (std::sin(value_divided_by_3) / value_divided_by_3);
     }
     return 0.0f;
 }
